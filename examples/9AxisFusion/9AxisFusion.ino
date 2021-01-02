9f6d31a8cad3c36928830ced2c3d28ff7d683266
Twometer <twometer@outlook.de>
2021-01-02 15:58:55 +0100

    Indent with spaces not tabs

--- examples/9AxisFusion/9AxisFusion.ino
+++ examples/9AxisFusion/9AxisFusion.ino
@@ -7,57 +7,57 @@ ICM20948 icm;
 
 void fail()
 {
-	delay(10000);
-	ESP.restart();
+    delay(10000);
+    ESP.restart();
 }
 
 void setup()
 {
-	Serial.begin(115200);
-	Wire.begin(PIN_SDA, PIN_SCL);
-	int code = icm.begin();
+    Serial.begin(115200);
+    Wire.begin(PIN_SDA, PIN_SCL);
+    int code = icm.begin();
 
-	switch (code)
-	{
-	case ICM_SUCCESS:
-		Serial.println("Init ok");
-		break;
-	case ICM_BAD_WHOAMI:
-		Serial.println("Bad WHOAMI");
-		fail();
-		break;
-	case ICM_DMP_ERROR:
-		Serial.println("Failed to initialize DMP");
-		fail();
-		break;
-	case ICM_MAG_ERROR:
-		Serial.println("Magnetometer not found");
-		fail();
-		break;
-	case ICM_SERIAL_ERROR:
-		Serial.println("Serial connection failure");
-		fail();
-		break;
-	case ICM_SETUP_ERROR:
-		Serial.println("Device setup failure");
-		fail();
-		break;
-	}
+    switch (code)
+    {
+    case ICM_SUCCESS:
+        Serial.println("Init ok");
+        break;
+    case ICM_BAD_WHOAMI:
+        Serial.println("Bad WHOAMI");
+        fail();
+        break;
+    case ICM_DMP_ERROR:
+        Serial.println("Failed to initialize DMP");
+        fail();
+        break;
+    case ICM_MAG_ERROR:
+        Serial.println("Magnetometer not found");
+        fail();
+        break;
+    case ICM_SERIAL_ERROR:
+        Serial.println("Serial connection failure");
+        fail();
+        break;
+    case ICM_SETUP_ERROR:
+        Serial.println("Device setup failure");
+        fail();
+        break;
+    }
 
-	icm.startSensor(INV_SENSOR_TYPE_MAGNETOMETER, 16000);
-	icm.startSensor(INV_SENSOR_TYPE_GAME_ROTATION_VECTOR, 16000);
-	icm.startSensor(INV_SENSOR_TYPE_ROTATION_VECTOR, 16000);
+    icm.startSensor(INV_SENSOR_TYPE_MAGNETOMETER, 16000);
+    icm.startSensor(INV_SENSOR_TYPE_GAME_ROTATION_VECTOR, 16000);
+    icm.startSensor(INV_SENSOR_TYPE_ROTATION_VECTOR, 16000);
 }
 
 void loop()
 {
-	icm.update();
+    icm.update();
 
-	if (icm.available())
-	{
-		Serial.printf("%f %f %f %f\n", icm.x(), icm.y()), icm.z(), icm.w());
-		icm.clearAvailable();
-	}
+    if (icm.available())
+    {
+        Serial.printf("%f %f %f %f\n", icm.x(), icm.y()), icm.z(), icm.w());
+        icm.clearAvailable();
+    }
 
-	delay(16);
+    delay(16);
 }
--- src/ICM20948.h
+++ src/ICM20948.h
@@ -15,12 +15,12 @@
 
 #define EXPECTED_WHOAMI 0xEA
 
-#define ICM_SUCCESS 		0	/* init successful */
-#define ICM_BAD_WHOAMI 		-1  /* device reported wrong whoami */
-#define ICM_MAG_ERROR 		-2  /* compass sensor not found */
-#define ICM_SERIAL_ERROR 	-3  /* serial connection failed */
-#define ICM_SETUP_ERROR		-4  /* failed to setup device */
-#define ICM_DMP_ERROR		-5  /* failed to upload dmp fw */
+#define ICM_SUCCESS 0       /* init successful */
+#define ICM_BAD_WHOAMI -1   /* device reported wrong whoami */
+#define ICM_MAG_ERROR -2    /* compass sensor not found */
+#define ICM_SERIAL_ERROR -3 /* serial connection failed */
+#define ICM_SETUP_ERROR -4  /* failed to setup device */
+#define ICM_DMP_ERROR -5    /* failed to upload dmp fw */
 
 static const uint8_t dmp3_image[] = {
 #include "Invn/Images/icm20948_img.dmp3a.h"
@@ -29,111 +29,111 @@ static const uint8_t dmp3_image[] = {
 class ICM20948
 {
 private:
-	inv_device_icm20948_t device_icm20948;
-	inv_device_t *device;
+    inv_device_icm20948_t device_icm20948;
+    inv_device_t *device;
 
-	volatile bool available = false;
-	volatile float quat[] = {1, 0, 0, 0};
+    volatile bool available = false;
+    volatile float quat[] = {1, 0, 0, 0};
 
-	static void sensor_event_cb(const inv_sensor_event_t *event, void *context)
-	{
-		ICM20948 *icm = (ICM20948 *)context;
+    static void sensor_event_cb(const inv_sensor_event_t *event, void *context)
+    {
+        ICM20948 *icm = (ICM20948 *)context;
 
-		if (event->status == INV_SENSOR_STATUS_DATA_UPDATED)
-		{
-			if (INV_SENSOR_ID_TO_TYPE(event->sensor) == INV_SENSOR_TYPE_ROTATION_VECTOR)
-			{
-				icm->quat[0] = event->data.quaternion.quat[0];
-				icm->quat[1] = event->data.quaternion.quat[1];
-				icm->quat[2] = event->data.quaternion.quat[2];
-				icm->quat[3] = event->data.quaternion.quat[3];
-				icm->available = true;
-			}
-		}
-	}
+        if (event->status == INV_SENSOR_STATUS_DATA_UPDATED)
+        {
+            if (INV_SENSOR_ID_TO_TYPE(event->sensor) == INV_SENSOR_TYPE_ROTATION_VECTOR)
+            {
+                icm->quat[0] = event->data.quaternion.quat[0];
+                icm->quat[1] = event->data.quaternion.quat[1];
+                icm->quat[2] = event->data.quaternion.quat[2];
+                icm->quat[3] = event->data.quaternion.quat[3];
+                icm->available = true;
+            }
+        }
+    }
 
 public:
-	int begin()
-	{
-		inv_host_serif_t serif_instance = {
-			idd_io_hal_init_i2c,
-			0,
-			idd_io_hal_read_reg,
-			idd_io_hal_write_reg,
-			0,
-			32, /* max read transact size */
-			32, /* max write transact size */
-			INV_HOST_SERIF_TYPE_I2C,
-		};
+    int begin()
+    {
+        inv_host_serif_t serif_instance = {
+            idd_io_hal_init_i2c,
+            0,
+            idd_io_hal_read_reg,
+            idd_io_hal_write_reg,
+            0,
+            32, /* max read transact size */
+            32, /* max write transact size */
+            INV_HOST_SERIF_TYPE_I2C,
+        };
 
-		inv_sensor_listener_t sensor_listener = {
-			sensor_event_cb, /* callback func */
-			this			 /* context */
-		};
+        inv_sensor_listener_t sensor_listener = {
+            sensor_event_cb, /* callback func */
+            this             /* context */
+        };
 
-		if (inv_host_serif_open(&serif_instance))
-			return ICM_SERIAL_ERROR;
+        if (inv_host_serif_open(&serif_instance))
+            return ICM_SERIAL_ERROR;
 
-		inv_device_icm20948_init(&device_icm20948, &serif_instance, &sensor_listener, dmp3_image, sizeof(dmp3_image));
-		device = inv_device_icm20948_get_base(&device_icm20948);
+        inv_device_icm20948_init(&device_icm20948, &serif_instance, &sensor_listener, dmp3_image, sizeof(dmp3_image));
+        device = inv_device_icm20948_get_base(&device_icm20948);
 
-		uint8_t whoami = 0xff;
-		inv_device_whoami(device, &whoami);
-		if (whoami != EXPECTED_WHOAMI)
-			return ICM_BAD_WHOAMI;
+        uint8_t whoami = 0xff;
+        inv_device_whoami(device, &whoami);
+        if (whoami != EXPECTED_WHOAMI)
+            return ICM_BAD_WHOAMI;
 
-		if (inv_device_setup(device))
-			return ICM_SETUP_ERROR;
+        if (inv_device_setup(device))
+            return ICM_SETUP_ERROR;
 
-		if (inv_device_load(device, NULL, dmp3_image, sizeof(dmp3_image), true /* verify */, NULL))
-			return ICM_DMP_ERROR;
+        if (inv_device_load(device, NULL, dmp3_image, sizeof(dmp3_image), true /* verify */, NULL))
+            return ICM_DMP_ERROR;
 
-		if (inv_device_ping_sensor(device, INV_SENSOR_TYPE_UNCAL_MAGNETOMETER))
-			return ICM_MAG_ERROR;
+        if (inv_device_ping_sensor(device, INV_SENSOR_TYPE_UNCAL_MAGNETOMETER))
+            return ICM_MAG_ERROR;
 
-		return ICM_SUCCESS;
-	}
+        return ICM_SUCCESS;
+    }
 
-	void startSensor(int type, int period_us)
-	{
-		inv_device_set_sensor_period_us(device, type, period_us);
-		inv_device_start_sensor(device, type);
-	}
+    void startSensor(int type, int period_us)
+    {
+        inv_device_set_sensor_period_us(device, type, period_us);
+        inv_device_start_sensor(device, type);
+    }
 
-	void update()
-	{
-		inv_device_poll(device);
-	}
+    void update()
+    {
+        inv_device_poll(device);
+    }
 
-	bool available()
-	{
-		return available;
-	}
+    bool available()
+    {
+        return available;
+    }
 
-	void clearAvailable()
-	{
-		available = false;
-	}
+    void clearAvailable()
+    {
+        available = false;
+    }
 
-	float x()
-	{
-		return quat[0];
-	}
+    float x()
+    {
+        return quat[0];
+    }
 
-	float y()
-	{
-		return quat[1];
-	}
+    float y()
+    {
+        return quat[1];
+    }
 
-	float z()
-	{
-		return quat[2];
-	}
+    float z()
+    {
+        return quat[2];
+    }
 
-	float w()
-	{
-		return quat[3];
-	}
+    float w()
+    {
+        return quat[3];
+    }
 };
 
 #endif
