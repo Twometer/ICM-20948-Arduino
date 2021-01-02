2e6fd53f763ce19de63a84cf698cfad7dcdc53dc
Twometer <twometer@outlook.de>
2021-01-02 15:56:44 +0100

    Update 9 axis fusion example

--- examples/9AxisFusion/9AxisFusion.ino
+++ examples/9AxisFusion/9AxisFusion.ino
@@ -1,57 +1,63 @@
 #include <ICM20948.h>
 
-#define PIN_SDA	 	4
-#define PIN_SCL 	5
+#define PIN_SDA 4
+#define PIN_SCL 5
 
 ICM20948 icm;
 
-void fail() {
+void fail()
+{
 	delay(10000);
 	ESP.restart();
 }
 
-void setup() {
+void setup()
+{
 	Serial.begin(115200);
 	Wire.begin(PIN_SDA, PIN_SCL);
 	int code = icm.begin();
 
-	switch (code) {
-		case ERR_OK:
-			Serial.println("Init ok");
-			break;
-		case ERR_BAD_WHOAMI:
-			Serial.println("Bad WHOAMI");
-			fail();
-			break;
-		case ERR_DMP_FAILED:
-			Serial.println("Failed to initialize DMP");
-			fail();
-			break;
-		case ERR_AUX_FAILED:
-			Serial.println("Auxiliary devices not found");
-			fail();
-			break;
+	switch (code)
+	{
+	case ICM_SUCCESS:
+		Serial.println("Init ok");
+		break;
+	case ICM_BAD_WHOAMI:
+		Serial.println("Bad WHOAMI");
+		fail();
+		break;
+	case ICM_DMP_ERROR:
+		Serial.println("Failed to initialize DMP");
+		fail();
+		break;
+	case ICM_MAG_ERROR:
+		Serial.println("Magnetometer not found");
+		fail();
+		break;
+	case ICM_SERIAL_ERROR:
+		Serial.println("Serial connection failure");
+		fail();
+		break;
+	case ICM_SETUP_ERROR:
+		Serial.println("Device setup failure");
+		fail();
+		break;
 	}
 
-	icm.enableSensor(INV_SENSOR_TYPE_GYROSCOPE);
-	icm.enableSensor(INV_SENSOR_TYPE_ACCELEROMETER);
-	icm.enableSensor(INV_SENSOR_TYPE_MAGNETOMETER);
-	icm.enableSensor(INV_SENSOR_TYPE_ROTATION_VECTOR);
-	icm.setSampleRate(INV_SENSOR_TYPE_ROTATION_VECTOR, 60);
+	icm.startSensor(INV_SENSOR_TYPE_MAGNETOMETER, 16000);
+	icm.startSensor(INV_SENSOR_TYPE_GAME_ROTATION_VECTOR, 16000);
+	icm.startSensor(INV_SENSOR_TYPE_ROTATION_VECTOR, 16000);
 }
 
-void loop() {
+void loop()
+{
 	icm.update();
 
-	if (icm.available()) {
-		vec4 &quat = icm.getRotation();
-		Serial.print(quat.x);
-		Serial.print(", ");
-		Serial.print(quat.y);
-		Serial.print(", ");
-		Serial.print(quat.z);
-		Serial.print(", ");
-		Serial.println(quat.w);
+	if (icm.available())
+	{
+		Serial.printf("%f %f %f %f\n", icm.x(), icm.y()), icm.z(), icm.w());
 		icm.clearAvailable();
 	}
+
+	delay(16);
 }
